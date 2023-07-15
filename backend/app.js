2263dd755ddff3ff0b3d81f32ff54c2f8f2bc7b7
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-07-15 22:25:24 +0300

    -debug

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-error');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
