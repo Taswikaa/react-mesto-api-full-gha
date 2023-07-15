ef2b37e49423dbc9829368f9abbfcd4f548835c5
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-07-15 21:39:21 +0300

    -debug

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,6 +7,16 @@ const allowedCors = [
 // eslint-disable-next-line func-names
 module.exports = function (req, res, next) {
   const { origin } = req.headers;
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+  }
 
   if (allowedCors.includes(origin)) {
     res.headers('Access-Control-Allow-Origin', origin);
