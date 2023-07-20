953c58529daab4c00b1b0fdae808b4c3bb206eaa
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-07-20 19:06:35 +0300

    debug

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -122,10 +122,6 @@ module.exports.login = (req, res, next) => {
       // eslint-disable-next-line no-shadow
       const { password, ...userWithoutPassword } = user;
 
-      console.log(user.password);
-      console.log('123');
-      console.log(userWithoutPassword.password);
-
       res.send(userWithoutPassword);
     })
     .catch(next);
